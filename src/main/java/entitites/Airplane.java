e2b5304e892cab157c8cfd57a19304c7481d7cc4
arisromil <arisromil@yahoo.com>
2023-09-10 00:07:35 -0600

    simple entitites

---
+++ src/main/java/entitites/Airplane.java
@@ -0,0 +1,15 @@
+package entitites;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Airplane {
+
+    @Id
+    private String model;
+    private String manufacturer;
+    private String nationality;
+    private int seats;
+
+}
---
+++ src/main/java/entitites/Developer.java
@@ -0,0 +1,14 @@
+package entitites;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Developer {
+
+    @Id
+    private String nickname;
+    private String name;
+    private String city;
+    private String language;
+}
