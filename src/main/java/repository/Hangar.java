5b82ac7812d64fdc6fd9c7049f3563713e22dbb1
arisromil <arisromil@yahoo.com>
2023-09-11 00:38:50 -0600

    Hangar repository

---
+++ src/main/java/repository/Hangar.java
@@ -0,0 +1,12 @@
+package repository;
+
+import entitites.Airplane;
+import jakarta.data.repository.Page;
+import jakarta.data.repository.Pageable;
+import jakarta.data.repository.PageableRepository;
+import jakarta.data.repository.Repository;
+
+@Repository
+public interface Hangar extends PageableRepository<Airplane, String> {
+    Page<Hangar> findByManufacturer(String manufacturer, Pageable pageable);
+}
